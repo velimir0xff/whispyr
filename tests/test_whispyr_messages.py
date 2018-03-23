3c78740f38619bb2f2a117ab3c3808c19938d985
Grigory Starinkin <starinkin@gmail.com>
2018-03-23 12:00:53 +0000

    add messages list tests

--- tests/test_whispyr_messages.py
+++ tests/test_whispyr_messages.py
@@ -3,6 +3,7 @@
 
 """Tests for `whispyr` package"""
 
+from collections import Iterable, MutableMapping
 from urllib.parse import urlparse
 
 
@@ -29,6 +30,27 @@ def test_send_alias_creates_message(whispir, cassete):
         subject='whispyr test',
         body='test message, please disregard')
     _check_message_created(message, cassete)
+
+
+def test_list_messages_for_root_workspace(whispir, cassete):
+    messages = whispir.messages.list()
+    _check_list_messages(messages)
+
+
+def test_list_messages(whispir, cassete):
+    workspace = whispir.workspaces.Workspace(id='8080DE5434485ED4')
+    messages = workspace.messages.list()
+    _check_list_messages(messages)
+
+
+def _check_list_messages(messages):
+    assert isinstance(messages, Iterable)
+    messages = list(messages)
+    assert len(messages) > 0
+    for message in messages:
+        assert isinstance(message, MutableMapping)
+        assert 'id' in message
+        assert 'subject' in message
 
 
 def _check_message_created(message, cassete):
