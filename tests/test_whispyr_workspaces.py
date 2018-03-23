d79157e1fccd276d6eacc39d41f3c8868ca95e2a
Grigory Starinkin <starinkin@gmail.com>
2018-03-23 14:09:39 +0000

    add workspace list tests

---
+++ tests/test_whispyr_workspaces.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+"""Tests for `whispyr` workspaces"""
+
+from collections import Iterable, MutableMapping
+
+
+def test_create_workspace(cassette, whispir):
+    workspace = whispir.workspaces.create(
+        projectName='whispyr tests', status='A')
+    assert isinstance(workspace, MutableMapping)
+    assert 'id' in workspace
+
+
+def test_list_workspaces(cassette, whispir):
+    workspaces = whispir.workspaces.list()
+    assert isinstance(workspaces, Iterable)
+    workspaces = list(workspaces)
+    assert len(workspaces) > 0
+    for workspace in workspaces:
+        assert isinstance(workspace, MutableMapping)
+        assert 'id' in workspace
+        assert 'projectName' in workspace
