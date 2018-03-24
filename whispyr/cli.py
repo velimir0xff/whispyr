bdb94fe12e548546dd32b60fec1b2f2ef33bc607
Grigory Starinkin <starinkin@gmail.com>
2018-03-24 15:15:03 +0000

    add ability to configure client through config file
    add coloured output for CLI

--- whispyr/cli.py
+++ whispyr/cli.py
@@ -2,16 +2,18 @@
 
 """Console script for whispyr."""
 
-import sys
 import click
+import configparser
 import json
-import pprint
 import logging
+import os
+import sys
 
-import whispyr
+from pygments import highlight, lexers, formatters
 from whispyr import Whispir
 
 LOGGING_LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR]
+
 
 def setup_logging(verbosity):
     level = LOGGING_LEVELS[-(verbosity + 1)]
@@ -33,26 +35,87 @@ def setup_logging(verbosity):
     requests_log.propagate = True
 
 
-@click.group()
+def config_paths(app_name='whispyr', file_name='config.ini'):
+    for force_posix in [True, False]:
+        app_dir = click.get_app_dir(app_name, force_posix=force_posix)
+        yield os.path.join(app_dir, file_name)
+
+
+class ClickSection:
+
+    def __init__(self, section):
+        self.section = section
+
+    def get(self, key):
+        section = self.section
+        return section.get(key, fallback=None)
+
+
+class ClickConfig:
+
+    def __init__(self, config, section_name=None):
+        self.config = config
+        self._section = section_name or config.default_section
+
+    def get(self, key):
+        config = self.config
+        section = config[key] if key in config else None
+        if section:
+            return ClickSection(section)
+        return config.get(self._section, key, fallback=None)
+
+
+def read_config():
+    config = configparser.ConfigParser(default_section='default')
+    paths = config_paths()
+    config.read(paths)
+    return ClickConfig(config)
+
+
+CONTEXT_SETTINGS = dict(default_map=read_config())
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.option('-u', '--username', prompt=True)
 @click.password_option('-p', '--password', confirmation_prompt=False)
 @click.password_option('-a', '--api-key', confirmation_prompt=False)
-@click.option('-w', '--workspace', prompt=True)
-@click.version_option()
+@click.option('-w', '--workspace')
 @click.option('-v', 'verbosity', count=True, default=0,
               type=click.IntRange(0, len(LOGGING_LEVELS) - 1, clamp=True))
+@click.version_option()
 @click.pass_context
 def cli(ctx, username, password, api_key, workspace, verbosity):
     """Command line interface for Whispir API"""
     setup_logging(verbosity)
-    ctx.obj = Whispir(username, password, api_key, workspace=workspace)
+    ctx.obj = Whispir(username, password, api_key)
 
 
 @cli.group()
 @click.pass_context
 def messages(ctx):
-    """send all sorts of messages"""
+    """send and view all sorts of messages"""
     ctx.obj = ctx.obj.messages
+
+
+@cli.group()
+@click.pass_context
+def workspaces(ctx):
+    """create and view workspaces"""
+    ctx.obj = ctx.obj.workspaces
+
+
+@workspaces.command()
+@click.pass_obj
+def list(workspaces):
+    """get list of workspaces"""
+    echo_json(dict_list(workspaces.list()))
+
+
+@messages.command()
+@click.pass_obj
+def list(messages):
+    """get list of recent messages"""
+    echo_json(dict_list(messages.list()))
+
 
 @messages.group()
 @click.pass_obj
@@ -68,7 +131,18 @@ def send(messages):
 def sms(messages, **kwargs):
     """send SMS"""
     resp = messages.create(**kwargs)
-    click.echo(pprint.pformat(resp))
+    echo_json(resp)
+
+
+def echo_json(obj):
+    formatted = json.dumps(obj, indent=2)
+    colored = highlight(
+        formatted, lexers.JsonLexer(), formatters.TerminalFormatter())
+    click.echo(colored)
+
+
+def dict_list(items):
+    return [dict(it) for it in items]
 
 
 def valid_json(ctx, param, value):
